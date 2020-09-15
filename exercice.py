59d8a0cae1ca558be52717fd302e11936bbd0f0c
bASILgIL <basil.gil@polymtl.ca>
2020-09-15 14:03:20 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -16,13 +16,13 @@ def average(a: float, b: float, c: float) -> float:
     return (a + b + c)/3
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return 
+    return math.radians(angle_degs + angle_mins/60 + angle_secs/3600)
 
 
 def to_degrees(angle_rads: float) -> tuple:
     degrees = math.degrees(angle_rads)
-    minutes = (degrees %1)*60
-    secondes = (minutes %1)*60
+    minutes = (degrees)*60
+    secondes = (minutes)*60
     return degrees, minutes, secondes
 
 
